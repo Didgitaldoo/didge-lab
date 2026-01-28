896cbbfa5285dcd900da3f901bcad7709f22dcbc
Jan Nehring <jan.nehring@giz.de>
2026-01-28 18:40:20 +0200

    update

--- src/README.md
+++ src/README.md
@@ -1,6 +1,15 @@
 # How to create the python package
 
 ```
+python setup.py build_ext --inplace
+```
+
+```
+python setup.py build_ext
+python setup.py install --user
+```
+
+```
 python setup.py sdist bdist_wheel
 pip install .
 ```
--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packagesp, Extension
 from Cython.Build import cythonize
 import numpy
 
@@ -9,19 +9,26 @@ setup(
     url='https://github.com/Didgitaldoo/didgelab',
     author='Jan Nehring',
     author_email='jan.nehring@outlook.com',
-    license='GNU GENERAL PUBLIC LICENSE v2',
+    license='Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International',
     include_package_data=True,
     packages=["didgelab", "didgelab.calc", "didgelab.calc.sim", "didgelab.evo", "didgelab.util"],
-    install_requires=["configargparse==1.5.3",
-        "pandas==1.5.0",
-        "scipy==1.9.1",
-        "matplotlib==3.6.1",
-        "prettytable==3.4.1",
-        "seaborn==0.12.0",
-        "tqdm==4.64.1",
-        "Cython==0.29.32",
-        "flask==2.3.2",
+    setup_requires=[
+        'setuptools>=18.0',  # Setuptools 18.0+ properly handles Cython extensions
+        'cython==0.29.32',
     ],
+    install_requires=['configargparse==1.5.3',
+        'pandas==1.5.0',
+        'scipy==1.10.1',
+        'matplotlib==3.6.1',
+        'prettytable==3.4.1',
+        'seaborn==0.12.0',
+        'tqdm==4.64.1',
+        'cython==0.29.32',
+        'flask==2.3.2',
+        'jsonlines==4.0.0',
+        'setuptools>=18.0',  # Setuptools 18.0+ properly handles Cython extensions
+        ]
+    ,
     classifiers=[
         'Programming Language :: Python :: 3.8',
     ],
